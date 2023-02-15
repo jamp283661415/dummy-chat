eff415d361883589960da2636c95842e86888ed2
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-02-14 22:16:58 -0800

    style the chat

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import cx from "classnames";
 
 function App() {
   const [message, setMesssage] = useState("");
@@ -12,6 +13,7 @@ function App() {
       content: "Yes? How may I help?",
     },
   ]);
+  const chatAreaRef = useRef();
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -31,28 +33,36 @@ function App() {
           content: "Bot Message here",
         },
       ]);
-    }, 1000)
+
+      setTimeout(() => {
+        chatAreaRef.current.scrollTo(0, chatAreaRef.current.scrollHeight);
+      }, 500);
+    }, 1000);
     setMesssage("");
+    
   };
 
   const handleMessageChange = (e) => {
     setMesssage(e.target.value);
-  }
+  };
 
   return (
     <div className="App">
       <div className="container">
-        <div className="h-50">
-          <ul
-            className="list-group list-group-flush"
-            style={{ height: "500px", overflowY: "scroll" }}
-          >
-            {chat.map((item, id) => (
-              <li className="list-group-item" key={id}>
-                {item.char}: {item.content}
-              </li>
-            ))}
-          </ul>
+        <h1>Dummy Chat</h1>
+        <p>A demo of a chat simulation with no AI.</p>
+        <div className="h-50 my-3" style={{ maxHeight: "500px", overflowY: "scroll" }} ref={chatAreaRef}>
+          {chat.map((item, id) => (
+            <div
+              className={cx(
+                "py-1 px-2 rounded my-2 border",
+                item.char === "You" ? "text-end bg-success text-light" : "bg-body-secondary text-dark"
+              )}
+              key={id}
+            >
+              {item.content}
+            </div>
+          ))}
         </div>
         <form onSubmit={handleFormSubmit}>
           <div className="input-group mb-3">
@@ -65,7 +75,11 @@ function App() {
               onChange={handleMessageChange}
               value={message}
             />
-            <button className="input-group-text" id="basic-addon2" type="submit">
+            <button
+              className="input-group-text"
+              id="basic-addon2"
+              type="submit"
+            >
               Send
             </button>
           </div>
